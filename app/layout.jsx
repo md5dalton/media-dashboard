a136be68617d6a789232b0fc896eb1ae87920be0
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-05-13 12:53:32 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,19 +7,22 @@ import "./App.sass"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Social Media Dashboard",
-  description: "Social Media Dashboard with NextJS",
+    title: "Social Media Dashboard",
+    description: "Social Media Dashboard with NextJS",
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />    
-        <main>
-            {children}
-        </main>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en" className={`${inter.className}`}>
+            <body className="
+            px-6 pb-20 mx-auto max-w-[1110px] md:p-0
+            text-very-dark-blue dark:text-white dark:bg-very-dark-blue
+            ">
+            <Header />    
+            <main>
+                {children}
+            </main>
+            </body>
+        </html>
+    )
 }
