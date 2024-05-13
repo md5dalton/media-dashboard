fafc04a9a60d3161147de6e5037d734846ff9582
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-05-13 13:20:46 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,22 @@
+import User from "@/components/User"
+import UList from "@/components/UList"
+import { details, users } from "@/data"
+import Details from "@/components/Details"
+
+import "./page.sass"
+
 export default () => (
-    <p>page</p>
+    <div className="space-y-10">
+        <UList
+            items={users}
+            itemHandler={item => <User {...item} />}
+        />
+        <div className="overview">
+            <h2>Overview - Today</h2>
+            <UList
+                items={details}
+                itemHandler={item => <Details {...item} />}
+            />
+        </div>
+    </div>
 )
