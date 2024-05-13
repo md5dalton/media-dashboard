58361102be46abf81ee756cd992f371ef15daed5
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-05-13 13:22:11 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,77 @@
+export const users = [
+    {
+        username: "@nathanf",
+        followers: 1987,
+        change: 12,
+        platform: "facebook"
+    },
+    {
+        username: "@nathanf",
+        followers: 1044,
+        change: 99,
+        platform: "twitter"
+    },
+    {
+        username: "@realnathanf",
+        followers: "11k",
+        change: 1099,
+        platform: "instagram"
+    },
+    {
+        username: "Nathan F",
+        followers: 8239,
+        change: -144,
+        platform: "youtube"
+    }
+]
+
+export const details = [
+    {
+        param: "page views",
+        platform: "facebook",
+        count: 87,
+        change: 3
+    },
+    {
+        param: "likes",
+        platform: "facebook",
+        count: 52,
+        change: -2
+    },
+    {
+        param: "likes",
+        platform: "instagram",
+        count: 5462,
+        change: 2257
+    },
+    {
+        param: "profile views",
+        platform: "instagram",
+        count: "52k",
+        change: 1375
+    },
+    {
+        param: "retweets",
+        platform: "twitter",
+        count: 117,
+        change: 303
+    },
+    {
+        param: "likes",
+        platform: "twitter",
+        count: 507,
+        change: 553
+    },
+    {
+        param: "likes",
+        platform: "youtube",
+        count: 107,
+        change: -19
+    },
+    {
+        param: "total views",
+        platform: "youtube",
+        count: 1407,
+        change: -12
+    }
+]
