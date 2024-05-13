7368c051f44841434e4b521cc20505c796cf836a
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-05-13 12:53:34 +0200

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,9 +2,9 @@ import Toggle from "./Toggle"
 
 export default () => (
     <header className="my-9 md:flex md:justify-between">
-        <div>
+        <div className="font-bold">
             <h1>Social Media Dashboard</h1>
-            <p className="text-sm font-bold text-dark-grayish-blue dark:text-desaturated-blue">Total followers: 23,005</p>
+            <p className="text-sm text-dark-grayish-blue dark:text-desaturated-blue">Total followers: 23,005</p>
         </div>
         <Toggle />
     </header>
